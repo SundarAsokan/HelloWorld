658a584aaa02be8da04125ca1b5a2993b8ece11c
sundarasokan <sundarasokan@yahoo.com>
2020-06-13 18:50:56 -0400

    Updates

---
+++ precache-manifest.77cd8dbdec254831bd5e2efb25975a15.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb105d1ce58b7a967aab14fda38433f3",
+    "url": "/HelloWorld/index.html"
+  },
+  {
+    "revision": "ccb1300482e95e95268d",
+    "url": "/HelloWorld/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3c72a5803aca8300f7be",
+    "url": "/HelloWorld/static/js/2.2139f4a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/HelloWorld/static/js/2.2139f4a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccb1300482e95e95268d",
+    "url": "/HelloWorld/static/js/main.c4765097.chunk.js"
+  },
+  {
+    "revision": "2d41bb7b4026904ee0c0",
+    "url": "/HelloWorld/static/js/runtime-main.df4f36eb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/HelloWorld/static/media/logo.5d5d9eef.svg"
+  }
+]);
